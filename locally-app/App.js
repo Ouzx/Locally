52211d990676f9c12ec096ece0b41843d06de4a7
sinemablak <87177874+sinemablak@users.noreply.github.com>
2021-07-09 16:50:51 +0300

    Co-authored-by: Oğuzhan Kandakoğlu <Ouzx@users.noreply.github.com>

--- locally-app/App.js
+++ locally-app/App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {Test} from './src/test';
+const asd = () => (console.log('asdas'))
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>vhs</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableOpacity onPress={asd}>
+      <Test />
+    </TouchableOpacity>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
